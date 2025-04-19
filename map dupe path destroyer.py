a4a3f1ac00d9a62d18f9a1f4181b9e8c1c43d113
Ed <67418738+lmoadeck-Lunity@users.noreply.github.com>
2025-04-19 19:29:10 +0800

    i forgot objects

--- map dupe path destroyer.py
+++ map dupe path destroyer.py
@@ -28,6 +28,26 @@ for eachmap in ls:
                 with open(eachmap, 'w', encoding='utf-16', errors='ignore') as f:
                     f.writelines(maap)
                 print(f"Updated path: {'\\'.join(new_path)}")
+        
+        if maap[count] == "[object]\n":
+            patha = maap[count + 2]
+            # print(patha)
+            patha = patha.split("\\")
+            # print(patha)
+            fields = Counter(patha)
+            duplicates = [field for field, count in fields.items() if count > 1]
+            if duplicates:
+                seen = set()
+                new_path = []
+                for folder in patha:
+                    if folder not in seen:
+                        seen.add(folder)
+                        new_path.append(folder)
+                print(f"{'\\'.join(new_path)}")
+                maap[count + 2] = "\\".join(new_path)
+                with open(eachmap, 'w', encoding='utf-16', errors='ignore') as f:
+                    f.writelines(maap)
+                print(f"Updated path: {'\\'.join(new_path)}")
             # print(f"{'\\'.join(duplicates)}\\{patha[-1]}")
 
         count += 1
