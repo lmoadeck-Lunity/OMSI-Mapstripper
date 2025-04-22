d6efd3b5a8426230a2e3c6e75a518e1d304cd91f
Ed <67418738+lmoadeck-Lunity@users.noreply.github.com>
2025-04-23 01:35:07 +0800

    ok i fixed the spline bug

--- map dupe path destroyer.py
+++ map dupe path destroyer.py
@@ -2,14 +2,15 @@ import os
 from collections import Counter
 
 ls = os.listdir()
-ls = [os.path.abspath(x) for x in ls if os.path.isfile(x) and x.endswith('.map')]
+ls_map = [os.path.abspath(x) for x in ls if os.path.isfile(x) and x.endswith('.map')]
+ls_prt = [os.path.abspath(x) for x in ls if os.path.isfile(x) and x.endswith('.prt')]
 # print(ls)
-for eachmap in ls:
+for eachmap in ls_map:
     maap = open(eachmap, 'r', encoding='utf-16-le',errors='ignore').readlines()
     # print(maap)
     count = 0
     while count < len(maap):
-        if maap[count] == "[spline]\n":
+        if maap[count] in ['[spline_h]\n', '[spline]\n','[object]\n']:
             patha = maap[count + 2]
             # print(patha)
             patha = patha.split("\\")
@@ -29,11 +30,41 @@ for eachmap in ls:
                     f.writelines(maap)
                 print(f"Updated path: {'\\'.join(new_path)}")
         
-        if maap[count] == "[object]\n":
-            patha = maap[count + 2]
-            # print(patha)
+
+            # print(f"{'\\'.join(duplicates)}\\{patha[-1]}")
+
+        count += 1
+    # break
+    # for eachline in maap:
+    #     if eachline == "[spline]":
+for eachprt in ls_prt:
+    prt = open(eachprt, 'r', encoding='utf-16-le',errors='ignore').readlines()
+    # print(maap)
+    count = 0
+    # while count < len(prt):
+    #     if prt[count] in ['[spline_h]\n', '[spline]\n','[object]\n']:
+    #         patha = prt[count + 2]
+    #         # print(patha)
+    #         patha = patha.split("\\")
+    #         # print(patha)
+    #         fields = Counter(patha)
+    #         duplicates = [field for field, count in fields.items() if count > 1]
+    #         if duplicates:
+    #             seen = set()
+    #             new_path = []
+    #             for folder in patha:
+    #                 if folder not in seen:
+    #                     seen.add(folder)
+    #                     new_path.append(folder)
+    #             print(f"{'\\'.join(new_path)}")
+    #             prt[count + 2] = "\\".join(new_path)
+    #             with open(eachprt, 'w', encoding='utf-16', errors='ignore') as f:
+    #                 f.writelines(prt)
+    #             print(f"Updated path: {'\\'.join(new_path)}")
+    for eachline in prt:
+        if eachline.isalnum() and not eachline.isdigit():
+            patha = eachline
             patha = patha.split("\\")
-            # print(patha)
             fields = Counter(patha)
             duplicates = [field for field, count in fields.items() if count > 1]
             if duplicates:
@@ -44,14 +75,14 @@ for eachmap in ls:
                         seen.add(folder)
                         new_path.append(folder)
                 print(f"{'\\'.join(new_path)}")
-                maap[count + 2] = "\\".join(new_path)
-                with open(eachmap, 'w', encoding='utf-16', errors='ignore') as f:
-                    f.writelines(maap)
+                prt[count] = "\\".join(new_path)
+                with open(eachprt, 'w', encoding='utf-16', errors='ignore') as f:
+                    f.writelines(prt)
                 print(f"Updated path: {'\\'.join(new_path)}")
+                
+        
+
             # print(f"{'\\'.join(duplicates)}\\{patha[-1]}")
 
         count += 1
-    # break
-    # for eachline in maap:
-    #     if eachline == "[spline]":
 pause = input("Press Enter to continue...")
